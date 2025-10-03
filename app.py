4af68fbe36743e04a6ae881f53fd856c239b3c11
NicolasGayb <nicolasgbleal@gmail.com>
2025-10-03 15:05:43 -0300

    primeiro commit

---
+++ app.py
@@ -0,0 +1,9 @@
+print('Bem-vindo ao Sabor Express!\n')
+
+print('1 - Cadastrar restaurante')
+print('2 - Listar restaurantes')
+print('3 - Ativar restaurante')
+print('4 - Sair\n')
+
+opcao_usuario = input('Escolha uma opção: ')
+print('Você escolheu a opção:', opcao_usuario)
