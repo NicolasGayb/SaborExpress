64df2ed5ad5f5accbb4b4580b66c35fc80228335
NicolasGayb <nicolasgbleal@gmail.com>
2025-10-03 15:51:03 -0300

    separando funções

--- app.py
+++ app.py
@@ -1,31 +1,44 @@
 import os
 
-print("""
-██████████████████████████████████████████████████████████████████████████
-█─▄▄▄▄██▀▄─██▄─▄─▀█─▄▄─█▄─▄▄▀███▄─▄▄─█▄─▀─▄█▄─▄▄─█▄─▄▄▀█▄─▄▄─█─▄▄▄▄█─▄▄▄▄█
-█▄▄▄▄─██─▀─███─▄─▀█─██─██─▄─▄████─▄█▀██▀─▀███─▄▄▄██─▄─▄██─▄█▀█▄▄▄▄─█▄▄▄▄─█
-▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▀▀▄▄▄▄▀▄▄▀▄▄▀▀▀▄▄▄▄▄▀▄▄█▄▄▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀
+def nome_programa():
+    print("""
+    ██████████████████████████████████████████████████████████████████████████
+    █─▄▄▄▄██▀▄─██▄─▄─▀█─▄▄─█▄─▄▄▀███▄─▄▄─█▄─▀─▄█▄─▄▄─█▄─▄▄▀█▄─▄▄─█─▄▄▄▄█─▄▄▄▄█
+    █▄▄▄▄─██─▀─███─▄─▀█─██─██─▄─▄████─▄█▀██▀─▀███─▄▄▄██─▄─▄██─▄█▀█▄▄▄▄─█▄▄▄▄─█
+    ▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▀▀▄▄▄▄▀▄▄▀▄▄▀▀▀▄▄▄▄▄▀▄▄█▄▄▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀
 
-""")
+    """)
 
-print('1 - Cadastrar restaurante')
-print('2 - Listar restaurantes')
-print('3 - Ativar restaurante')
-print('4 - Sair\n')
-
-opcao_usuario = input('Escolha uma opção: ')
-opcao_usuario = int(opcao_usuario)
+def exibir_opções():
+    print('1 - Cadastrar restaurante')
+    print('2 - Listar restaurantes')
+    print('3 - Ativar restaurante')
+    print('4 - Sair\n')
 
 def finalizar_app():
     os.system('cls')
-    print('Finalizando o app...')
+    print('Finalizando o app...\n')
     exit()
 
-if opcao_usuario == 1:
-    print('Cadastrar restaurante')
-elif opcao_usuario == 2:
-    print('Listar restaurantes')
-elif opcao_usuario == 3:
-    print('Ativar restaurante')
-else:
-    finalizar_app()
+def escolher_opcao():
+    opcao_usuario = input('Escolha uma opção: ')
+    opcao_usuario = int(opcao_usuario)
+
+    if opcao_usuario == 1:
+        cadastro_restaurante()
+        print('Cadastrar restaurante')
+    elif opcao_usuario == 2:
+        print('Listar restaurantes')
+    elif opcao_usuario == 3:
+        print('Ativar restaurante')
+    else:
+        finalizar_app()
+
+def main():
+    os.system('cls')
+    nome_programa()
+    exibir_opções()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
